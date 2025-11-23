061833b1550066c64bbef9927c3ed6a0d25ffc3c
Ronin Duck <63336301+Murqin@users.noreply.github.com>
2025-11-23 20:18:29 +0300

    Update build script to use env Python directly
    Modified build.bat to call env\Scripts\python.exe directly instead of activating the virtual environment. Added a comment explaining the rationale for this change to streamline the build process.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-call .venv\Scripts\activate.bat
-python -m PyInstaller --noconsole --onefile --name="Specific Tool" --clean --uac-admin --icon="assets/specific-tool.ico" main.py
+env\Scripts\python.exe -m PyInstaller --noconsole --onefile --name="Specific Tool" --clean --uac-admin --icon="assets/specific-tool.ico" main.py 
+rem The reason I use it like this: .venv\Scripts\python.exe -m is because I'm a bit lazy and want to run the script and get a quick build.
 echo Build Complete.
 pause
